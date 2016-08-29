a64448e84fe85d85edc3097f3788717e06fadfbb
Aye Min Aung <mgdevine89@gmail.com>
2016-08-30 00:00:05 +0630

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by Aye Min Aung on 8/30/16.
+//  Copyright © 2016 Aye Min Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
